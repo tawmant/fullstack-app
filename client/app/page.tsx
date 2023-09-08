803016674fd46a30a04d2dcd07dd859fb7a0a63b
Azamat Askarov <azamat.17111711@gmail.com>
2023-09-08 14:17:52 +0600

    change hero page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>home</div>
+  return (
+    <div>
+      <h1>home</h1>
+      <p className="font-bold text-red-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      </p>
+    </div>
+  )
 }
 
 export default Home
